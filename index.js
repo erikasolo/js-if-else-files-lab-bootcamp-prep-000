4fac597e9e814258f70446cb38ebffe40192aa50
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-13 14:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ if (name === "Susan") {
 } else{
   console.log("Expected the name to be Susan")
 }
-
+if (height === "70") {
+  console.log ("The height is correct")
+} else{
+  console.log("Expected the height to be 70")
+}
 
 var name = "Susan"
 var height = "70"
